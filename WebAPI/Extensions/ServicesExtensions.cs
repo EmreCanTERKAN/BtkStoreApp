79746b3cfb317c3b1f2388a56ea7b3b717ce68e6
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-02-19 16:41:55 +0300

    ServiceExtentions

---
+++ WebAPI/Extensions/ServicesExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Repositories.EFCore;
+
+namespace WebAPI.Extensions;
+
+public static class ServicesExtensions
+{
+    public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) => services.AddDbContext<RepositoryContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("sqlConnecttion")));
+
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(setup =>
+        {
+            var jwtSecuritySheme = new OpenApiSecurityScheme
+            {
+                BearerFormat = "JWT",
+                Name = "JWT Authentication",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                Description = "Put **_ONLY_** yourt JWT Bearer token on textbox below!",
+
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            setup.AddSecurityDefinition(jwtSecuritySheme.Reference.Id, jwtSecuritySheme);
+
+            setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { jwtSecuritySheme, Array.Empty<string>() }
+                });
+        });
+    }
+
+
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,43 +1,15 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using Repositories.EFCore;
 using WebAPI.Endpoints;
+using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(setup =>
-{
-    var jwtSecuritySheme = new OpenApiSecurityScheme
-    {
-        BearerFormat = "JWT",
-        Name = "JWT Authentication",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = JwtBearerDefaults.AuthenticationScheme,
-        Description = "Put **_ONLY_** yourt JWT Bearer token on textbox below!",
 
-        Reference = new OpenApiReference
-        {
-            Id = JwtBearerDefaults.AuthenticationScheme,
-            Type = ReferenceType.SecurityScheme
-        }
-    };
-
-    setup.AddSecurityDefinition(jwtSecuritySheme.Reference.Id, jwtSecuritySheme);
-
-    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    { jwtSecuritySheme, Array.Empty<string>() }
-                });
-});
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<RepositoryContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnecttion"));
-});
-builder.Services.AddScoped<RepositoryContext>();
+
+builder.Services.ConfigureSwagger();
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 
 var app = builder.Build();
