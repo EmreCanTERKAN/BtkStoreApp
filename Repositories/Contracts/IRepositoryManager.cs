224bd8aa3d8e614b372ae03afda2bc18b6d76a30
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-02-19 15:00:35 +0300

    Repository Manager uygulaması

---
+++ Repositories/Contracts/IRepositoryManager.cs
@@ -0,0 +1,6 @@
+﻿namespace Repositories.Contracts;
+public interface IRepositoryManager
+{
+    IBookRepository Book { get; }
+    void Save();
+}
---
+++ Repositories/EFCore/RepositoryManager.cs
@@ -0,0 +1,12 @@
+﻿using Repositories.Contracts;
+
+namespace Repositories.EFCore;
+public class RepositoryManager(RepositoryContext context) : IRepositoryManager
+{
+    public IBookRepository Book => new BookRepository(context);
+
+    public void Save()
+    {
+        context.SaveChanges();
+    }
+}
