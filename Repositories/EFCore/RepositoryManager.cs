9fb7d5dfaaf64a99c8ccd67a8a19488154a03f36
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-02-19 15:24:00 +0300

    Lazy loading.

--- Repositories/EFCore/RepositoryManager.cs
+++ Repositories/EFCore/RepositoryManager.cs
@@ -1,12 +1,21 @@
 ﻿using Repositories.Contracts;
 
 namespace Repositories.EFCore;
-public class RepositoryManager(RepositoryContext context) : IRepositoryManager
+public class RepositoryManager : IRepositoryManager
 {
-    public IBookRepository Book => new BookRepository(context);
+    private readonly RepositoryContext _context;
+    private readonly Lazy<IBookRepository> _bookRepository;
+
+    public RepositoryManager(RepositoryContext context)
+    {
+        _context = context;
+        _bookRepository = new Lazy<IBookRepository>(() => new BookRepository(_context));
+    }
+
+    public IBookRepository Book => _bookRepository.Value;
 
     public void Save()
     {
-        context.SaveChanges();
+        _context.SaveChanges();
     }
 }
