88652a3801e28def1b12b01c8b789305e39bb164
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-02-20 20:16:27 +0300

    ILoggingService

---
+++ Services/Contracts/ILoggerService.cs
@@ -0,0 +1,8 @@
+﻿namespace Services.Contracts;
+public interface ILoggerService
+{
+    Task LogAsync(string message);
+    Task LogWarningAsync(string message);
+    Task LogErrorAsync(string message);
+    Task LogDebugAsync(string message);
+}
