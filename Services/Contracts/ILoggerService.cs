e97077f25a2d36946c6d35f1c3a0ebb683b5fe17
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-02-20 21:33:18 +0300

    Logger Manager

--- Services/Contracts/ILoggerService.cs
+++ Services/Contracts/ILoggerService.cs
@@ -1,7 +1,7 @@
 ﻿namespace Services.Contracts;
 public interface ILoggerService
 {
-    Task LogAsync(string message);
+    Task LogInfoAsync(string message);
     Task LogWarningAsync(string message);
     Task LogErrorAsync(string message);
     Task LogDebugAsync(string message);
---
+++ Services/LoggerManager.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using Services.Contracts;
+
+namespace Services;
+public class LoggerManager : ILoggerService
+{
+    private static ILogger logger = LogManager.GetCurrentClassLogger();
+    public Task LogInfoAsync(string message)
+    {
+        logger.Info(message);
+        return Task.CompletedTask;
+    }
+
+    public Task LogDebugAsync(string message)
+    {
+        logger.Debug(message);
+        return Task.CompletedTask;
+    }
+
+
+    public Task LogErrorAsync(string message)
+    {
+        logger.Error(message);
+        return Task.CompletedTask;
+    }
+
+    public Task LogWarningAsync(string message)
+    {
+        logger.Warn(message);
+        return Task.CompletedTask;
+    }
+}
