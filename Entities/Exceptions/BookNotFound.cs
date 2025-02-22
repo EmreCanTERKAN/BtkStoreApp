1b0f3fb819f19029bde415078217aeb678462a95
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-02-22 21:06:05 +0300

    Not Found Exception

---
+++ Entities/Exceptions/BookNotFound.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions;
+
+public sealed class  BookNotFound : NotFound
+{
+    public BookNotFound(int id) : base($"{id}li kitap bulunamadı..")
+    {
+        
+    }
+}
---
+++ Entities/Exceptions/NotFound.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Exceptions;
+public abstract class NotFound : Exception
+{
+    protected NotFound(string message) : base(message)
+    {
+
+    }
+}
